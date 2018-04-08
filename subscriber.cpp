9be7569c5146170ea789ee2d2c211b2f54952837
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-04-07 20:48:36 -0500

    Renamed the test subscriber (tests are done)
    * the subsciber will fliker the light sub recieved package

--- subscriber.cpp
+++ subscriber.cpp
@@ -1,28 +1,192 @@
-/**
- * Simple subscriber that will await a message from the SUBSCRIPTION_URI
- * then print to screen and exit
- */
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <zmq.hpp>
-#include <iostream>
 #include <sstream>
+#include <iostream>
 
-#define SUBSCRIPTION_URI "tcp://localhost:5556"
+#define URI "tcp://127.0.0.1:5556"
 
-int main()
+#define IN  0
+#define OUT 1
+
+#define LOW  0
+#define HIGH 1
+
+#define PIN  24 /* P1-18 */
+#define POUT 4  /* P1-07 */
+
+//=================== Fucntions to manage GPIO =================================
+static int
+GPIOExport(int pin)
 {
-  zmq::context_t context (1);
+#define BUFFER_MAX 3
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
 
-  std::cout << "Awaiting the message Sir!\n" << std::endl;
-  zmq::socket_t subscriber (context, ZMQ_SUB);
-  subscriber.connect(SUBSCRIPTION_URI);
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open export for writing!\n");
+		return(-1);
+	}
 
-  subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
-  zmq::message_t alert(2);
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return(0);
+}
 
-  subscriber.recv(&alert);
+static int
+GPIOUnexport(int pin)
+{
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
 
-  std::cout << "AH SHIT WE'RE HIT SIR!\n" << std::endl;
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open unexport for writing!\n");
+		return(-1);
+	}
 
-  return 0;
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return(0);
+}
+
+static int
+GPIODirection(int pin, int dir)
+{
+	static const char s_directions_str[]  = "in\0out";
+
+#define DIRECTION_MAX 35
+	char path[DIRECTION_MAX];
+	int fd;
+
+	snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+	fd = open(path, O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio direction for writing!\n");
+		return(-1);
+	}
+
+	if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+		fprintf(stderr, "Failed to set direction!\n");
+		return(-1);
+	}
+
+	close(fd);
+	return(0);
+}
+
+static int
+GPIORead(int pin)
+{
+#define VALUE_MAX 30
+	char path[VALUE_MAX];
+	char value_str[3];
+	int fd;
+
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	fd = open(path, O_RDONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio value for reading!\n");
+		return(-1);
+	}
+
+	if (-1 == read(fd, value_str, 3)) {
+		fprintf(stderr, "Failed to read value!\n");
+		return(-1);
+	}
+
+	close(fd);
+
+	return(atoi(value_str));
+}
+
+static int
+GPIOWrite(int pin, int value)
+{
+	static const char s_values_str[] = "01";
+
+	char path[VALUE_MAX];
+	int fd;
+
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	fd = open(path, O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Failed to open gpio value for writing!\n");
+		return(-1);
+	}
+
+	if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
+		fprintf(stderr, "Failed to write value!\n");
+		return(-1);
+	}
+
+	close(fd);
+	return(0);
+}
+
+//========================== Main Loop =========================================
+
+int
+main(int argc, char *argv[])
+{
+	/*
+	 * Enable GPIO pins
+	 */
+	if (-1 == GPIOExport(POUT) || -1 == GPIOExport(PIN))
+		return(1);
+
+	/*
+	 * Set GPIO directions
+	 */
+	if (-1 == GPIODirection(POUT, OUT) || -1 == GPIODirection(PIN, IN))
+		return(2);
+
+	/*
+   * Set up ZMQ to communcate
+   */
+	zmq::context_t context (1);
+	zmq::socket_t subscriber (context, ZMQ_SUB);
+	subscriber.connect(URI);
+	subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+
+  bool repeat = false;
+	do {
+    /*
+     *  Await message
+     */
+		zmq::message_t ping;
+		subscriber.recv(&ping);
+
+		/*
+		 * Write GPIO value
+		 */
+		if (-1 == GPIOWrite(POUT, repeat))
+			return(3);
+
+		/*
+		 * Read GPIO value
+		 */
+		printf("I'm reading %d in GPIO %d\n", GPIORead(PIN), PIN);
+
+		usleep(500 * 1000);
+    repeat = !repeat;
+	}
+	while (true);
+
+	/*
+	 * Disable GPIO pins
+	 */
+	if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
+		return(4);
+
+	return(0);
 }
